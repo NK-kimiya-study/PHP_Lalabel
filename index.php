57465d0f410c2a6c0bdc7f09564d40146512cfdf
kimi <programassets6@gmail.com>
2024-04-17 15:33:36 +0900

    四則演算

--- index.php
+++ index.php
@@ -1,49 +1,14 @@
 <?php
-//連想配列
-$array_member = [
-    'name' => '本田',
-    'height' => 170,
-    'hobby' => 'サッカー'
-];
+//四則演算
+$test_1 = 7;
+$test_2 = 3;
 
-echo $array_member['hobby'];
+echo $test_1 + $test_2;
 echo '<br>';
-
-$array_member_2 = [
-    '本田' => [
-        'height' => 170,
-        'hobby' => 'サッカー'
-    ],
-    '田中' => [
-        'height' => 160,
-        'hobby' => '野球'
-    ]
-];
-
-echo $array_member_2['田中']['height'];
+echo $test_1 - $test_2;
 echo '<br>';
-
-$array_member_3 = [
-    'class1' => [
-        '本田' => [
-            'height' => 170,
-            'hobby' => 'サッカー'
-        ],
-        '田中' => [
-            'height' => 160,
-            'hobby' => '野球'
-        ]
-    ],
-    'class2' => [
-        '山田' => [
-            'height' => 180,
-            'hobby' => 'バスケ'
-        ],
-        '石田' => [
-            'height' => 175,
-            'hobby' => '卓球'
-        ]
-    ]
-];
-
-echo $array_member_3['class2']['山田']['height'];
+echo $test_1 * $test_2;
+echo '<br>';
+echo $test_1 / $test_2;
+echo '<br>';
+echo $test_1 % $test_2;
