07c53de007349fd0daefb60715382dec32cead3a
kimi <programassets6@gmail.com>
2024-04-17 14:46:52 +0900

    定数

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
 
-//変数 動的型付
-$test = 123;
-//$test = 'テスト';
-
-var_dump($test);
-
-$test_1 = 123;
-$test_2 = 456;
-
-$test_3 = $test_1 . $test_2;
-var_dump($test_3);
-
-echo $test;
+//定数(大文字)
+const MAX = 10;
+echo MAX;
