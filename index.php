01fd1f28f1b265f98491e271fa06ffde809b41ed
kimi <programassets6@gmail.com>
2024-04-17 15:07:08 +0900

    配列

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
+//配列
+$array = ['テスト', 1, 2, 3];
 
-//定数(大文字)
-const MAX = 10;
-echo MAX;
+echo $array[1];
+echo '<pre>';
+var_dump($array);
+echo '<pre>';
+
+$array2 = [
+    ['R', 'G', 'B'],
+    [220, 150, 100]
+];
+
+echo '<pre>';
+var_dump($array2);
+echo '<pre>';
+
+echo $array2[1][1];
