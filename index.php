311019d41dbbdcc891cdb86c4599459ef72d349e
kimi <programassets6@gmail.com>
2024-04-28 12:28:02 +0900

    switch

--- index.php
+++ index.php
@@ -1,65 +1,17 @@
 <?php
-//foreach
+//switch
+$data = '1';
 
-$members = [
-    'name' => 'テスト',
-    'height' => 170,
-    'hobby' => "サッカー"
-];
-
-//バリューのみ
-foreach ($members as $member) {
-    echo $member;
-}
-
-//キーとバリュー
-foreach ($members as $key => $value) {
-    echo $key . 'は' . $value . 'です';
-}
-
-//多段階のforeach
-$members_2 = [
-    'テスト' => [
-        'height' => 170,
-        'hobby' => "サッカー"
-    ],
-    'テスト2' => [
-        'height' => 165,
-        'hobby' => "サッカー"
-    ]
-];
-
-foreach ($members_2 as $member_1) {
-    foreach ($member_1 as $member) {
-        echo $member;
-    }
-}
-
-//for(繰り返す回数が決まってる場合)
-//while(繰り返す回数が決まってない場合)
-for ($i = 0; $i < 10; $i++) {
-    echo $i;
-}
-
-//continue,break
-for ($i = 0; $i < 10; $i++) {
-    if ($i === 5) {
+switch ($data) {
+    case $data === 1:
+        echo '1';
         break;
-    }
-    echo $i;
-}
-
-for ($i = 0; $i < 10; $i++) {
-    if ($i === 5) {
-        continue;
-    }
-    echo $i;
-}
-
-echo '<br>';
-
-$j = 0;
-while ($j < 5) {
-    echo $j;
-    $j++;
+    case 2:
+        echo '2';
+        break;
+    case 3:
+        echo '3';
+        break;
+    default:
+        echo 'default';
 }
