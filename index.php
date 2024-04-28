7bff044360d19ea713aa2461d45544fc844374ca
kimi <programassets6@gmail.com>
2024-04-28 13:12:40 +0900

    組み込み関数の自作

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
 <?php
-$test = 'test';
-echo strlen($test);
+$postalCode = '123-4567';
+//camelCase
+function checkPostalCode($str)
+{
+    $replaced = str_replace('-', '', $str);
+    $length = strlen($replaced);
 
-$str = '文字列を置換する';
-echo str_replace('置換', 'ちかん', $str);
+    if ($length === 7) {
+        return true;
+    }
+    return false;
+}
 
-$str_2 = '指定文字列で、分割';
-echo '<br/>';
-var_dump(explode('、', $str_2));
-
-echo '<br/>';
-$str_3 = '特定の文字列が含まれているか確認する';
-echo preg_match('/文字列/', $str_3);
-
-echo '<br/>';
-echo substr('abcde', 1);
-
-//配列に配列を追加する
-$array = ['りんご', 'みかん'];
-array_push($array, 'ぶとう', 'なし');
-echo '<br/>';
-var_dump($array);
+var_dump(checkPostalCode($postalCode));
+//snakeCase
+//check_postal_code()
