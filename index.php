25e087d5bda4fadea0dd99c9fd2b13a2da9e6242
kimi <programassets6@gmail.com>
2024-04-28 13:03:53 +0900

    組み込み関数ー文字列、配列

--- index.php
+++ index.php
@@ -1,33 +1,23 @@
 <?php
-//ユーザー定義関数
-function test()
-{
-    echo 'テスト';
-}
+$test = 'test';
+echo strlen($test);
 
-test();
+$str = '文字列を置換する';
+echo str_replace('置換', 'ちかん', $str);
 
-$comment = 'コメント';
-function getComment($string)
-{
-    echo $string;
-}
+$str_2 = '指定文字列で、分割';
+echo '<br/>';
+var_dump(explode('、', $str_2));
 
-getComment($comment);
+echo '<br/>';
+$str_3 = '特定の文字列が含まれているか確認する';
+echo preg_match('/文字列/', $str_3);
 
-function getNumberOfComment()
-{
-    return 5;
-}
+echo '<br/>';
+echo substr('abcde', 1);
 
-$commentNumber = getNumberOfComment();
-echo $commentNumber;
-
-function sumPrice($int1, $int2)
-{
-    $int3 = $int1 + $int2;
-    return $int3;
-}
-
-$total = sumPrice(3, 5);
-echo $total;
+//配列に配列を追加する
+$array = ['りんご', 'みかん'];
+array_push($array, 'ぶとう', 'なし');
+echo '<br/>';
+var_dump($array);
